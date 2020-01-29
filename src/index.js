a231aa61bbf9941a16193519edeb97c026b6ed46
Tripple-A <biodun9@gmail.com>
2020-01-29 18:46:51 +0100

    added the to-do-list

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@
 
 const Project = (name, list) => ({ name, list });
 
+const List = (title, description, dueDate, priority) => ({
+  title, description, dueDate, priority,
+});
 
 Player1 = Project('read',[])
-Player1.list.push(2)
+list1 = List('buy house','save money', '23/6/9', 'low')
+Player1.list.push(list1)
 
-console.log(Player1)
+console.log(Player1.list)
