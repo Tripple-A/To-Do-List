8db36e0990df8a0862cba319b63ab5245a5af5c3
Tripple-A <biodun9@gmail.com>
2020-01-29 18:27:13 +0100

    added the project factory function

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
-import './style.css';
-console.log('hey')
+//import './style.css';
+
+const Project = (name, list) => ({ name, list });
+
+
+Player1 = Project('read',[])
+Player1.list.push(2)
+
+console.log(Player1)
