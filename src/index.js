7c040f37a5e02f388f09d364935f41c4fa3ef03c
Tripple-A <biodun9@gmail.com>
2020-01-30 13:45:20 +0100

    added form for adding new project and new list and function for moving the created items to array

--- src/index.js
+++ src/index.js
@@ -1,6 +1,43 @@
 import './style.css';
-import { Project, List } from './logic';
+import {
+  Project, List, Move, Itemize, 
+} from './logic';
+
+const mylist = [];
+const addProject = document.getElementById('add-project');
+const projectList = document.querySelector('.project-list');
+const newProj = document.querySelector('.add-proj');
+const create = document.querySelector('.create');
+const input = document.querySelector('.new-project');
+const cancel = document.querySelector('.cancel');
+
+const showList = () => {
+  mylist.forEach((item) => {
+    const li = document.createElement('li');
+    const a = document.createElement('a');
+    a.textContent = item.name;
+    a.href = '#';
+    li.appendChild(a);
+    projectList.appendChild(li);
+  });
+};
+
+function newProject() {
+  create.addEventListener('click', () => {
+    const newAdded = Project(input.value, []);
+    Move(newAdded, mylist);
+    newProj.classList.add('no-display');
+    showList();
+  });
+
+  addProject.addEventListener('click', () => {
+    newProj.classList.remove('no-display');
+  });
+}
 
 
 const p = Project('My first Project', []);
-document.querySelector('h1').textContent = p.name;
+Move(p, mylist);
+showList();
+newProject();
+console.log(mylist);
--- src/logic.js
+++ src/logic.js
@@ -4,4 +4,12 @@ const List = (title, description, dueDate, priority) => ({
   title, description, dueDate, priority,
 });
 
-export { Project, List };
+const Move = (item, array) => array.push(item);
+const Itemize = (array) => {
+  array.forEach((element) => {
+    return element.name;
+  });
+};
+export {
+  Project, List, Move, Itemize,
+};
